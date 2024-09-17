8d4968bebed9fc8d7fdeff90133f572db6049728
Kevin Nicolas <nicocastx@gmail.com>
2024-09-16 22:47:19 -0300

    version final 1.1 agregado limitador de peticiones para el envio de mail

--- src/main/java/com/portfolio/portfoliokevincastillo/controllers/IndexController.java
+++ src/main/java/com/portfolio/portfoliokevincastillo/controllers/IndexController.java
@@ -3,21 +3,29 @@ package com.portfolio.portfoliokevincastillo.controllers;
 
 import com.portfolio.portfoliokevincastillo.entities.dto.ProjectDto;
 import com.portfolio.portfoliokevincastillo.services.ProjectService;
-import com.portfolio.portfoliokevincastillo.services.ProjectServiceImpl;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.Duration;
 import java.util.List;
 
 @Controller
 public class IndexController {
     private final ProjectService projectService;
+    private final Bucket bucket;
 
     public IndexController(ProjectService projectService){
         this.projectService = projectService;
+        Bandwidth limit = Bandwidth.classic(2, Refill.greedy(2, Duration.ofMinutes(2)));
+        this.bucket = Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 
     @GetMapping("/")
@@ -36,18 +44,22 @@ public class IndexController {
     }
 
     @GetMapping("/contact")
-    public String contact(Model model){
+    public String contact(){
         return "contact";
     }
 
-    @PostMapping("contact")
+    @PostMapping("/contact")
     public String sendContactEmail(HttpServletRequest request){
-        projectService.sendEmail(
-                request.getParameter("sender"),
-                request.getParameter("subject"),
-                request.getParameter("body")
-        );
+        if (bucket.tryConsume(1)) {
+            projectService.sendEmail(
+                    request.getParameter("sender"),
+                    request.getParameter("subject"),
+                    request.getParameter("body")
+            );
 
-        return "redirect:/contact?success";
+            return "redirect:/contact?success";
+        }
+        return "redirect:/contact?retryIn2Minutes";
     }
+
 }
