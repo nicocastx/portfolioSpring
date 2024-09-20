a204498e2e0b412f678f50d9110e174daf1ba451
Kevin Nicolas <nicocastx@gmail.com>
2024-09-19 21:30:51 -0300

    version final 1.0 pero docker me hace renegar

--- src/main/java/com/portfolio/portfoliokevincastillo/repositories/ProjectRepositoryJson.java
+++ src/main/java/com/portfolio/portfoliokevincastillo/repositories/ProjectRepositoryJson.java
@@ -3,6 +3,8 @@ package com.portfolio.portfoliokevincastillo.repositories;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.portfolio.portfoliokevincastillo.entities.Project;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
@@ -13,6 +15,7 @@ import java.util.*;
 @Repository
 @Primary
 public class ProjectRepositoryJson implements ProjectRepository {
+    private final Logger log = LoggerFactory.getLogger(ProjectRepositoryJson.class);
 
     private List<Project> projects;
     private final ObjectMapper objectMapper;
@@ -26,12 +29,14 @@ public class ProjectRepositoryJson implements ProjectRepository {
     @PostConstruct
     public void init() {
         ClassPathResource classPathResource = new ClassPathResource(jsonFilePath);
-
         try {
+            log.info("Reading json file: " + classPathResource.getFile().getAbsolutePath());
             projects = Arrays.asList(objectMapper.readValue(classPathResource.getFile(), Project[].class));
+            log.info("Project array made and reading is completed");
         } catch (Exception e){
-            e.printStackTrace();
+            log.error("Exception while reading file: \n{}", e.getMessage());
         }
+
     }
 
     @Override
